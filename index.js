3fc7833cd8629dc160f64200a5b41842466e1baa
kibet nathan <nathanrufus540@gmail.com>
2023-09-18 22:39:05 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+var num1 =2;
+var num2 =31;
+var multiply = num1 * num2 ;
+
+
+let random = Math.floor(Math.random()*10)+ 1;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1,7,4,15,6,20);
